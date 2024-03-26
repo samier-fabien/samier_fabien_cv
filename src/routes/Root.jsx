7601aee564076341fcf593736862b7f4420ecd15
Samier Fabien <samierfabien@gmail.com>
2024-03-26 12:57:48 +0100

    root.jsx: rename Root.jsx + ajout photo et titre

---
+++ src/routes/Root.jsx
@@ -0,0 +1,23 @@
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <>
+      <div className="container-fluid bg-primary full-page">
+        <div className="row">
+          <div className="col position-absolute top-50 start-50 translate-middle text-center">
+            <img
+              src="../../src/assets/bitmaps/head-square.jpg"
+              className="rounded-circle border border-5 border-primary-subtle"
+              alt="Une photo de moi"
+            />
+            <h1 className="h1 m-4 text-light">Samier Fabien</h1>
+            <button type="button" class="btn btn-lg btn-outline-light">
+              Go !
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
