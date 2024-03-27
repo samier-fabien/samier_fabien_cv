1d3fd94128ad4a612d1fe27d4a3d3470bcf7f517
Samier Fabien <samierfabien@gmail.com>
2024-03-27 15:22:24 +0100

    Transition de Root vers autre page + animation au chargement de la page

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TransitionWrapper from "../components/TransitionWrapper";
 
 export default function Root() {
+  const [pageLoaded, setPageLoaded] = useState(false);
   const [hasClicked, setHasClicked] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoaded(true);
+    }, 1000);
+  }, []);
+
   return (
-    <TransitionWrapper
-      path={"/test"}
-      isInTransit={hasClicked}
-      transitionClass="fade-out"
-      transitionTime="300"
-      wrapperClass="container-fluid bg-primary full-page transition-opacity"
-    >
-      <div className="row">
-        <div className="col position-absolute top-50 start-50 translate-middle text-center text-light">
-          <img
-            src="../../src/assets/bitmaps/head-square.jpg"
-            className="rounded-circle shadow"
-            alt="Une photo de moi"
-          />
-          <h1 className="h1 mt-3">Samier Fabien</h1>
-          <h2 className="h5 mb-5">Concepteur, développeur d'applications</h2>
-          <button onClick={(e) => setHasClicked(true)} type="button" className="btn btn btn-light">
-            Jeter un œil
-          </button>
+    <div className="bg-primary">
+      <TransitionWrapper
+        path={"/test"}
+        isInTransit={hasClicked}
+        transitionClass="anim-fade-out"
+        transitionTime="500"
+        // wrapperClass="container-fluid bg-primary full-page transition-opacity"
+        wrapperClass="container-fluid bg-primary full-page"
+      >
+        <div className="row">
+          <div className="col position-absolute top-50 start-50 translate-middle text-center text-light">
+            <img
+              src="../../src/assets/bitmaps/head-square.jpg"
+              className="rounded-circle shadow anim-rotate-translate"
+              alt="Une photo de moi"
+            />
+            <h1 className="h1 mt-3 anim-fade-in">Samier Fabien</h1>
+            <h2 className="h5 mb-5 anim-fade-in">Concepteur, développeur d'applications</h2>
+            <button
+              onClick={(e) => setHasClicked(true)}
+              type="button"
+              className={pageLoaded ? "btn btn-light anim-jig" : "btn btn-light anim-right-inside"}
+            >
+              Jeter un œil
+            </button>
+          </div>
         </div>
-      </div>
-    </TransitionWrapper>
+      </TransitionWrapper>
+    </div>
   );
 }
