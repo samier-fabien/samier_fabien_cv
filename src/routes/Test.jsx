712b543c5aaf68e96f183b2b1166ac4e2544bb15
Samier Fabien <samierfabien@gmail.com>
2024-03-27 11:36:43 +0100

    Test: mise en forme basique

--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Test() {
-  return <div>Test</div>;
+  return (
+    <div className="container-fluid bg-primary full-page instantOutsideAnim">
+      <div className="row">
+        <div className="col position-absolute top-50 start-50 translate-middle text-center text-light">
+          <h1 className="h1 mt-3">Test Page</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
