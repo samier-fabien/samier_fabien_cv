a463452e6ca1786dd6428eb93f69d4be0ae17569
Samier Fabien <samierfabien@gmail.com>
2024-03-27 16:13:25 +0100

    Ajout style de base plus anim header

--- src/routes/Presentation.jsx
+++ src/routes/Presentation.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import TransitionWrapper from "../components/TransitionWrapper";
 
 export default function Presentation() {
-  return <div>Presentation</div>;
+  const [hasClicked, setHasClicked] = useState(false);
+
+  return (
+    <div className="bg-primary">
+      <TransitionWrapper
+        path={"/accueil"}
+        isInTransit={hasClicked}
+        transitionClass="anim-fade-out"
+        transitionTime="500"
+        // wrapperClass="container-fluid bg-primary full-page transition-opacity"
+        wrapperClass="container-fluid bg-primary full-page"
+      >
+        <header className="container m-3 p-5">
+          <div className="row">
+            <div className="col text-center text-light">
+              <img
+                src="../../src/assets/bitmaps/head-square.jpg"
+                className="rounded-circle shadow anim-translate-bounce"
+                alt="Une photo de moi"
+              />
+              <h1 className="h1 mt-3 anim-left-inside">Samier Fabien</h1>
+              <h2 className="h5 mb-5 anim-right-inside">Concepteur, développeur d'applications</h2>
+            </div>
+          </div>
+        </header>
+        <section className="container bg-white"></section>
+        <footer className="container bg-white"></footer>
+      </TransitionWrapper>
+    </div>
+  );
 }
