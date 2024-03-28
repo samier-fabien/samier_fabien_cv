cb774c5fcce6dbddb0e102486392a0d3c4c648f4
Samier Fabien <samierfabien@gmail.com>
2024-03-28 11:48:33 +0100

    Ajout BubblesContainer dans page de test

--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import BubblesContainer from "../components/BubblesContainer";
 
 export default function Test() {
+  const componentRef = useRef(null);
+  const [rowWidth, setRowWidth] = useState(0);
+
+  useEffect(() => {
+    setRowWidth(componentRef.current.offsetWidth);
+  }, []);
+
   return (
     <div className="container-fluid bg-primary full-page instantOutsideAnim">
       <div className="row">
-        <div className="col position-absolute top-50 start-50 translate-middle text-center text-light">
+        <div className="col text-center text-light">
           <h1 className="h1 mt-3">Test Page</h1>
+        </div>
+      </div>
+      <div className="row" ref={componentRef}>
+        <div className="col p-0">
+          <BubblesContainer w={rowWidth} h={500} />
         </div>
       </div>
     </div>
