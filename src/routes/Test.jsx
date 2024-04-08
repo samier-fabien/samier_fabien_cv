230d28f70a25192822fb9d8a57ea0912c04a3954
Samier Fabien <samierfabien@gmail.com>
2024-04-08 17:55:43 +0200

    détails cosmétiques

--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -15,11 +15,11 @@ export default function Test() {
         <div className="col p-0" ref={divRef}>
           <BubblesContainer
             parentRef={divRef}
-            h={1000}
+            h={800}
             framerate={30}
             speed={2}
-            minRadius={80}
-            maxRadius={140}
+            minRadius={40}
+            maxRadius={100}
             number={7}
             bubblesText={["Vite", "JS", "React", "CSS", "HTML", "PHP", "Sf"]}
           />
