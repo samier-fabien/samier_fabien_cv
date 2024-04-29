81611c6e5eff63ebf170698edbc4c64867883c35
Samier Fabien <samierfabien@gmail.com>
2024-04-29 19:10:52 +0200

    Ajout MenuListElement

---
+++ src/components/MenuListElement.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import Tooltip from "./Tooltip";
+import "../css/menuListElement.css";
+
+export default function MenuListElement({
+  iconName = "",
+  iconSize = "1rem",
+  tooltipText = "default",
+  hyperlinkClasses = "",
+}) {
+  const hyperlinkRef = useRef(null);
+
+  return (
+    <>
+      <a href="#" ref={hyperlinkRef} className={`menu-list-element ${hyperlinkClasses}`}>
+        <i className={`${iconName}`} style={{ fontSize: iconSize }}></i>
+      </a>
+      <Tooltip
+        targetRef={hyperlinkRef}
+        textToDisplay={tooltipText}
+        left="-6%"
+        top="-90%"
+        tooltipRadius="50"
+        tooltipClasses="text-dark"
+      />
+    </>
+  );
+}
