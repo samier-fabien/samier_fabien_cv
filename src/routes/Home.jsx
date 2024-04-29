949047d90fae23c0c58dcb9c1b885df1835d666f
Samier Fabien <samierfabien@gmail.com>
2024-04-29 19:35:11 +0200

    Inclusion du menu dans la page d'accueil

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import TransitionWrapper from "../components/TransitionWrapper";
+import RadialMenu from "../components/RadialMenu";
+import MenuListElement from "../components/MenuListElement";
+import "../css/menuButton.css";
 import "../css/home.css";
 
 export default function Home() {
@@ -22,29 +25,75 @@ export default function Home() {
         // wrapperClass="container-fluid bg-primary full-page transition-opacity"
         wrapperClass="bg-primary"
       >
-        <header className="container p-5 text-center">
+        <header className="container pb-5 mb-5 text-center">
           <div className="row">
-            <div className="col text-light">
-              <img
-                src="../../src/assets/bitmaps/head-square.jpg"
-                className="rounded-circle shadow anim-translate-bounce"
-                alt="Une photo de moi"
+            <div className="col text-dark d-block text-center">
+              <RadialMenu
+                elements={[
+                  <MenuListElement
+                    iconName="bi bi-gear"
+                    iconSize="3rem"
+                    tooltipText="Compétences"
+                    hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
+                  />,
+                  <MenuListElement
+                    iconName="bi bi-journals"
+                    iconSize="3rem"
+                    tooltipText="Expériences"
+                    hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
+                  />,
+                  <MenuListElement
+                    iconName="bi bi-code-slash"
+                    iconSize="3rem"
+                    tooltipText="Technologies"
+                    hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
+                  />,
+                  <MenuListElement
+                    iconName="bi bi-award"
+                    iconSize="3rem"
+                    tooltipText="Etudes"
+                    hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
+                  />,
+                  <MenuListElement
+                    iconName="bi bi-dribbble"
+                    iconSize="3rem"
+                    tooltipText="Hobbies"
+                    hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
+                  />,
+                ]}
+                firstItemAngleInRadians={Math.PI}
+                lastItemAngleInRadians={0}
+                menuRadius={130}
+                menuClasses="menu-button bg-primary"
+                listElementRadius={40}
+                listElementClasses="bg-primary text-white"
+                buttonRadius={60}
+                buttonHyperlinkClasses="d-inline-flex justify-content-center align-items-center"
+                button={
+                  <i className="bi bi-list text-dark text-white" style={{ fontSize: "5rem" }}></i>
+                }
+                duration={300}
+                delay={100}
               />
+            </div>
+          </div>
+        </header>
+        <section className="container">
+          <div className="row text-center">
+            <div className="col text-light">
               <h1 className="h1 mt-3 anim-left-inside">Samier Fabien</h1>
               <h2 className="h5 mb-5 anim-right-inside">Concepteur, développeur d'applications</h2>
             </div>
+            <h3 className="h3 anim-fade-in">
+              <i className="bi bi-quote me-3"></i>Logique et curieux, je suis soucieux de la bonne
+              exécution de mon travail ainsi que de sa{" "}
+              <pre className="d-inline">
+                qualité.
+                <i className="bi bi-quote ms-3 rotate-half"></i>
+              </pre>
+            </h3>
           </div>
-          <h3 className="h3 anim-fade-in">
-            <i className="bi bi-quote me-3"></i>Logique et curieux, je suis soucieux de la bonne
-            exécution de mon travail ainsi que de sa{" "}
-            <pre style={{ display: "inline" }}>
-              qualité.
-              <i className="bi bi-quote ms-3 rotate-half"></i>
-            </pre>
-          </h3>
-        </header>
-        <section className="container">
-          <h4 className="h1">
+          <h4 className="h4">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus nesciunt inventore
             voluptates! Voluptates, perspiciatis eveniet expedita fuga possimus culpa dolorum
             assumenda error fugiat consequuntur nobis eaque voluptas veniam iusto soluta? Lorem
--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -11,57 +11,6 @@ export default function Test() {
           <h1 className="h1 mt-3">Test Page</h1>
         </div>
       </div>
-      <div className="row">
-        <div className="col text-dark d-block text-center">
-          <RadialMenu
-            elements={[
-              <MenuListElement
-                iconName="bi bi-gear"
-                iconSize="3rem"
-                tooltipText="Compétences"
-                hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
-              />,
-              <MenuListElement
-                iconName="bi bi-journals"
-                iconSize="3rem"
-                tooltipText="Expériences"
-                hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
-              />,
-              <MenuListElement
-                iconName="bi bi-code-slash"
-                iconSize="3rem"
-                tooltipText="Technologies"
-                hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
-              />,
-              <MenuListElement
-                iconName="bi bi-award"
-                iconSize="3rem"
-                tooltipText="Etudes"
-                hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
-              />,
-              <MenuListElement
-                iconName="bi bi-dribbble"
-                iconSize="3rem"
-                tooltipText="Hobbies"
-                hyperlinkClasses="text-white text-center d-flex justify-content-center align-items-center"
-              />,
-            ]}
-            firstItemAngleInRadians={Math.PI}
-            lastItemAngleInRadians={0}
-            menuRadius={130}
-            menuClasses="menu-button bg-primary"
-            listElementRadius={40}
-            listElementClasses="bg-primary text-white"
-            buttonRadius={60}
-            buttonHyperlinkClasses="d-inline-flex justify-content-center align-items-center"
-            button={
-              <i className="bi bi-list text-dark text-white" style={{ fontSize: "5rem" }}></i>
-            }
-            duration={300}
-            delay={100}
-          />
-        </div>
-      </div>
     </div>
   );
 }
