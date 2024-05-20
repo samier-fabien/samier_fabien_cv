bec164e61c3c657d9261fc0263dc199900a6cf71
Samier Fabien <samierfabien@gmail.com>
2024-05-20 16:38:56 +0200

    Ajout de BubblesContainer

--- src/routes/Technologies.jsx
+++ src/routes/Technologies.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import BubblesContainer from "../components/bubbles/BubblesContainer";
 
 export default function Technologies() {
-  return <div>Technologies</div>;
+  const divRef = useRef(null);
+
+  return (
+    <div className="container-fluid bg-danger full-page instantOutsideAnim">
+      <div className="row">
+        <div className="col text-center text-light">
+          <h1 className="h1 mt-3">Test Page</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col p-0" ref={divRef}>
+          <BubblesContainer
+            parentRef={divRef}
+            h={800}
+            framerate={40}
+            speed={1.2}
+            minRadius={40}
+            maxRadius={100}
+            number={7}
+            bubblesText={["Vite", "JS", "React", "CSS", "HTML", "PHP", "Sf"]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
