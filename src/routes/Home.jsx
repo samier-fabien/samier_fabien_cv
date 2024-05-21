9ded8de5ca5401864bbb1b86c265ec5a5396e938
Samier Fabien <samierfabien@gmail.com>
2024-05-21 15:34:41 +0200

    Création de HeaderTransition et FooterTransition+déport du titre de la page dans HeaderTransition

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,66 +1,72 @@
 import React from "react";
+import HeaderTransition from "../components/HeaderTransition";
+import FooterTransition from "../components/FooterTransition";
 
 export default function Home() {
   return (
-    <section className="container bg-white text-dark">
-      <div className="row justify-content-center">
-        <div className="col-xxl-6 col-xl-8 col-lg-10">
-          <h3 className="display-5 text-center">
-            <span className="px-4 border-start border-5 border-info">En bref.</span>
-          </h3>
-          <p className="lead anim-fade-in p-4">
-            <i className="bi bi-quote me-2"></i>Logique et curieux, je suis soucieux de la bonne
-            exécution de mon travail et de sa qualité.
-            <i className="bi bi-quote ms-2"></i>
-          </p>
-          <p className="pb-5">
-            Je conçois et développe principalement des projets web. J'utilise pour cela html, css,
-            react et symfony entre autres. Je travaille sur linux et windows avec git et docker.
-            J'ai un an d'expérience et beaucoup de logique et de motivation.
-          </p>
-          <h3 className="display-5 text-center p-4">
-            <span className="px-4 border-start border-5 border-warning">
-              Plus exhaustivement.
-            </span>
-          </h3>
-          <h4 className="h4">La découverte.</h4>
-          <p>
-            C'est avec <strong>PHP 5.3</strong> que je me suis mis au développement. J'avais
-            découvert <strong>HTML et CSS</strong> précédemment au fil de ma curiosité et je
-            trouvais tout ça passionnant mais il me manquait de l'interactivité.{" "}
-            <strong>J'ai étudié l'algorithmie et les bases de données</strong> et après quelques
-            essais de forums je me suis mis à l'<strong>orienté objet</strong>, puis aux{" "}
-            <strong>design patterns</strong>.
-          </p>
-          <h4 className="h4 pt-4">Le développement dans mon métier d'alors.</h4>
-          <p>
-            <strong>C'était une passion</strong> quand j'ai changé d'orientation professionnelle
-            pour devenir usineur. Je me suis aperçu que j'avais de très bonnes bases lorsque j'ai
-            programmé pour les premières fois des machines à commande numérique. Le langage n'avait
-            rien en commun avec PHP, Javascript ou SQL mais l'algorithmie en procédurale avec des
-            boucles, des variables et des conditions, ça je connaissais.
-          </p>
-          <p>
-            <strong>Ma passion est devenu une force</strong> dans ce cadre professionnelle. Quand
-            j'ai évolué pour devenir contrôleur non-destructif, j'ai fais{" "}
-            <strong>quelques incursions en C++ et en Java</strong> pour faire notamment un programme
-            de calcul de balourd.
-          </p>
-          <h4 className="h4 pt-4">Développeur.</h4>
-          <p>
-            Lorsque <strong>j'ai eu l'opportunité faire de mon violon d'Ingres mon métier</strong>,
-            je l'ai saisie: remise à niveau puis{" "}
-            <strong>formation de Concepteur/Développeur d'Applications</strong> durant 10 mois. Une
-            fois le titre acquis j'ai pu faire mes armes.
-          </p>
-          <p>
-            Ma <strong>soif de connaissances</strong> et ma <strong>curiosité</strong> naturelles
-            sont ma grande force. Elles m'ont permis d'en apprendre toujours plus sur diverses
-            technologies et langages. <strong>Logique et curieux</strong>, je suis soucieux de la
-            bonne exécution de mon travail ainsi que de sa qualité.
-          </p>
+    <>
+      <HeaderTransition title="Présentation" />
+      <section className="container bg-white text-dark">
+        <div className="row justify-content-center">
+          <div className="col-xxl-6 col-xl-8 col-lg-10">
+            <h3 className="display-5 text-center">
+              <span className="px-4 border-start border-5 border-info">En bref.</span>
+            </h3>
+            <p className="lead anim-fade-in p-4">
+              <i className="bi bi-quote me-2"></i>Logique et curieux, je suis soucieux de la bonne
+              exécution de mon travail et de sa qualité.
+              <i className="bi bi-quote ms-2"></i>
+            </p>
+            <p className="pb-5">
+              Je conçois et développe principalement des projets web. J'utilise pour cela html, css,
+              react et symfony entre autres. Je travaille sur linux et windows avec git et docker.
+              J'ai un an d'expérience et beaucoup de logique et de motivation.
+            </p>
+            <h3 className="display-5 text-center p-4">
+              <span className="px-4 border-start border-5 border-warning">
+                Plus exhaustivement.
+              </span>
+            </h3>
+            <h4 className="h4">La découverte.</h4>
+            <p>
+              C'est avec <strong>PHP 5.3</strong> que je me suis mis au développement. J'avais
+              découvert <strong>HTML et CSS</strong> précédemment au fil de ma curiosité et je
+              trouvais tout ça passionnant mais il me manquait de l'interactivité.{" "}
+              <strong>J'ai étudié l'algorithmie et les bases de données</strong> et après quelques
+              essais de forums je me suis mis à l'<strong>orienté objet</strong>, puis aux{" "}
+              <strong>design patterns</strong>.
+            </p>
+            <h4 className="h4 pt-4">Le développement dans mon métier d'alors.</h4>
+            <p>
+              <strong>C'était une passion</strong> quand j'ai changé d'orientation professionnelle
+              pour devenir usineur. Je me suis aperçu que j'avais de très bonnes bases lorsque j'ai
+              programmé pour les premières fois des machines à commande numérique. Le langage
+              n'avait rien en commun avec PHP, Javascript ou SQL mais l'algorithmie en procédurale
+              avec des boucles, des variables et des conditions, ça je connaissais.
+            </p>
+            <p>
+              <strong>Ma passion est devenu une force</strong> dans ce cadre professionnelle. Quand
+              j'ai évolué pour devenir contrôleur non-destructif, j'ai fais{" "}
+              <strong>quelques incursions en C++ et en Java</strong> pour faire notamment un
+              programme de calcul de balourd.
+            </p>
+            <h4 className="h4 pt-4">Développeur.</h4>
+            <p>
+              Lorsque <strong>j'ai eu l'opportunité faire de mon violon d'Ingres mon métier</strong>
+              , je l'ai saisie: remise à niveau puis{" "}
+              <strong>formation de Concepteur/Développeur d'Applications</strong> durant 10 mois.
+              Une fois le titre acquis j'ai pu faire mes armes.
+            </p>
+            <p>
+              Ma <strong>soif de connaissances</strong> et ma <strong>curiosité</strong> naturelles
+              sont ma grande force. Elles m'ont permis d'en apprendre toujours plus sur diverses
+              technologies et langages. <strong>Logique et curieux</strong>, je suis soucieux de la
+              bonne exécution de mon travail ainsi que de sa qualité.
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <FooterTransition />
+    </>
   );
 }
--- src/routes/Technologies.jsx
+++ src/routes/Technologies.jsx
@@ -1,103 +1,116 @@
 import React, { useState, useRef, useEffect } from "react";
 import BubblesContainer from "../components/bubbles/BubblesContainer";
+import FooterTransition from "../components/FooterTransition";
+import HeaderTransition from "../components/HeaderTransition";
 
 export default function Technologies() {
   const divRef = useRef(null);
 
   return (
-    <div className="container bg-white">
-      <div className="row">
-        <div className="col p-0" ref={divRef}>
-          <BubblesContainer
-            parentRef={divRef}
-            h={600}
-            framerate={40}
-            speed={1.1}
-            minRadius={30}
-            maxRadius={80}
-            number={9}
-            bubblesText={["Vite", "JS", "React", "CSS", "HTML", "PHP", "Sf", "Docker", "Git"]}
-            bubblesColorOne="rgba(255, 255, 255, 0.3)"
-            bubblesColorTwo="rgba(240, 240, 255, 0.1)"
-            bubblesColorThree="rgba(100, 100, 180, 0.2)"
-            bubblesColorOneStop="0"
-            bubblesColorTwoStop="0.8"
-            bubblesColorThreeStop="1"
-          />
+    <>
+      <HeaderTransition title="Technologies Employées" />
+      <div className="container bg-white">
+        <div className="row">
+          <div className="col p-0" ref={divRef}>
+            <BubblesContainer
+              parentRef={divRef}
+              h={600}
+              framerate={40}
+              speed={1.1}
+              minRadius={30}
+              maxRadius={80}
+              number={9}
+              bubblesText={["Vite", "JS", "React", "CSS", "HTML", "PHP", "Sf", "Docker", "Git"]}
+              bubblesColorOne="rgba(255, 255, 255, 0.3)"
+              bubblesColorTwo="rgba(240, 240, 255, 0.1)"
+              bubblesColorThree="rgba(100, 100, 180, 0.2)"
+              bubblesColorOneStop="0"
+              bubblesColorTwoStop="0.8"
+              bubblesColorThreeStop="1"
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-xxl-6 col-xl-8 col-lg-10 text-dark">
+            <h2 className="display-6 pt-5">
+              <span className="px-4 border-start border-5 border-primary">Front-end.</span>
+            </h2>
+            <p>
+              Je travaille le plus couramment avec
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+                <li>React.js</li>
+                <li>Npm</li>
+              </ul>
+              Dans le cadre d'une application web "traditionnelle" dans laquelle le front et le back
+              sont gérés de manière monolithique, j'utilise
+              <ul>
+                <li>Twig</li>
+                <li>JQuery</li>
+                <li>htmx</li>
+              </ul>
+            </p>
+            <h2 className="display-6 pt-5">
+              <span className="px-4 border-start border-5 border-primary">Back-end.</span>
+            </h2>
+            <p>
+              Pour le back-end, j'utilise
+              <ul>
+                <li>PHP</li>
+                <li>Composer</li>
+                <li>Symfony</li>
+                <li>Api Platform</li>
+              </ul>
+            </p>
+            <h2 className="display-6 pt-5">
+              <span className="px-4 border-start border-5 border-primary">Outils.</span>
+            </h2>
+            <p>
+              C'est avec Trello que je m'organise. Dans la conception je m'appuie sur Draw.io pour
+              l'UML, Looping-mcd pour la base de données (merise) et Figma pour le maquettage.
+              <br />
+              Je me sers de VSCode en guise d'éditeur de code. Avec l'aide de Docker et Docker
+              Compose je conteneurise mes applications qui sont versionnées avec Git.
+              <br />
+              Quand il m'arrive de tester des parties de code simples, j'utilise en fonction des cas
+              l'extension LiveServer de VSCode, Laragon sur Windows ou un serveur web interne PHP
+              sur Linux. Pour tester mes API Rest et GraphQL, j'utilise Insomnia ou Postman.
+              <br />
+              Mes éléments graphiques sont principalement produits avec Inkscape, et Gimp (qui ont
+              remplacé Photoshop et Illustrator) ainsi que de générateurs en ligne.
+              <br />
+              Enfin, en guise de base de connaissances, j'ai utilisé OneNote dans un contexte
+              professionnel. J'utilise pour ma part Obsidian.
+            </p>
+            <h2 className="display-6 pt-5">
+              <span className="px-4 border-start border-5 border-primary">Brèves incursions.</span>
+            </h2>
+            <p>
+              Python me permet de gérer certaines tâches répétitives. C++ et Qt pour un programme de
+              calcul de balourd notamment. Java que j'affectionne particulièrement.
+            </p>
+            <h2 className="display-6 pt-5">
+              <span className="px-4 border-start border-5 border-primary">
+                Au fait, c'est quoi cette animation ?
+              </span>
+            </h2>
+            <p>
+              C'est un algorithme que j'ai créé et qui utilise une balise canvas pour dessiner des
+              sortes de "bulles" qui se collisionnent. J'avais juste une idée en tête et après
+              m'être beaucoup documenté j'ai traduit les équations vers le code.
+              <br />
+              On appelle cela des "collisions élastiques", l'energie et la vitesse sont conservées.
+              Les "bulles" ont toutes un diamètre et une vitesse de base aléatoirement attribués
+              (dans une certaine plage). Cela leur confère un poids et une inertie différents en
+              fonction de leur vitesse.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="row justify-content-center">
-        <div className="col-xxl-6 col-xl-8 col-lg-10 text-dark">
-          <h1 className="display-5 pb-5 text-center">
-            <span className="px-4 border-start border-5 border-primary">
-              Technologies employées.
-            </span>
-          </h1>
-          <h4 className="h4">Front-end.</h4>
-          <p>
-            Je travaille le plus couramment avec
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Bootstrap</li>
-              <li>React.js</li>
-              <li>Npm</li>
-            </ul>
-            Dans le cadre d'une application web "traditionnelle" dans laquelle le front et le back
-            sont gérés de manière monolithique, j'utilise
-            <ul>
-              <li>Twig</li>
-              <li>JQuery</li>
-              <li>htmx</li>
-            </ul>
-          </p>
-          <h4 className="h4">Back-end.</h4>
-          <p>
-            Pour le back-end, j'utilise
-            <ul>
-              <li>PHP</li>
-              <li>Composer</li>
-              <li>Symfony</li>
-              <li>Api Platform</li>
-            </ul>
-          </p>
-          <h4 className="h4">Outils.</h4>
-          <p>
-            C'est avec Trello que je m'organise. Dans la conception je m'appuie sur Draw.io pour
-            l'UML, Looping-mcd pour la base de données (merise) et Figma pour le maquettage.
-            <br />
-            Je me sers de VSCode en guise d'éditeur de code. Avec l'aide de Docker et Docker Compose
-            je conteneurise mes applications qui sont versionnées avec Git.
-            <br />
-            Quand il m'arrive de tester des parties de code simples, j'utilise en fonction des cas
-            l'extension LiveServer de VSCode, Laragon sur Windows ou un serveur web interne PHP sur
-            Linux. Pour tester mes API Rest et GraphQL, j'utilise Insomnia ou Postman.
-            <br />
-            Mes éléments graphiques sont principalement produits avec Inkscape, et Gimp (qui ont
-            remplacé Photoshop et Illustrator) ainsi que de générateurs en ligne.
-            <br />
-            Enfin, en guise de base de connaissances, j'ai utilisé OneNote dans un contexte
-            professionnel. J'utilise pour ma part Obsidian.
-          </p>
-          <h4 className="h4">Brèves incursions.</h4>
-          <p>
-            Python me permet de gérer certaines tâches répétitives. C++ et Qt pour un programme de
-            calcul de balourd notamment. Java que j'affectionne particulièrement.
-          </p>
-          <h4 className="h4">Rien à voir mais qu'en est-il de l'animation ?</h4>
-          <p>
-            C'est un algorithme que j'ai créé et qui utilise une balise canvas pour dessiner des
-            sortes de "bulles" qui se collisionnent. J'avais juste une idée en tête et après m'être
-            beaucoup documenté j'ai traduit les équations vers le code.
-            <br />
-            On appelle cela des "collisions élastiques": l'energie et la vitesse sont conservées.
-            Les "bulles" ont toutes un diamètre et une vitesse de base aléatoirement attribués (dans
-            une certaine plage). Cela leur confère un poids et une inertie différents en fonction de
-            leur vitesse.
-          </p>
-        </div>
-      </div>
-    </div>
+      <FooterTransition />
+    </>
   );
 }
