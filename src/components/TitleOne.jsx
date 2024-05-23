36873280278bef4176962474c5b36024b5bde852
Samier Fabien <samierfabien@gmail.com>
2024-05-23 17:38:55 +0200

    Ajout de composant TitleOne

---
+++ src/components/TitleOne.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TitleOne({ title = "", titleBorderColor = "black" }) {
+  return (
+    <h2 className="display-6 pt-5 text-center">
+      <span className="px-4" style={{ borderLeft: `0.3rem solid ${titleBorderColor}` }}>
+        {title}
+      </span>
+    </h2>
+  );
+}
