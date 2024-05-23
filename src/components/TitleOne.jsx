375eb2378e18179183fdb23e18cfd00a10c6d563
Samier Fabien <samierfabien@gmail.com>
2024-05-23 20:29:51 +0200

    Correction TitleOne padding

--- src/components/TitleOne.jsx
+++ src/components/TitleOne.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TitleOne({ title = "", titleBorderColor = "black" }) {
   return (
-    <h2 className="display-6 pt-5 text-center">
+    <h2 className="display-6 pt-3 text-center">
       <span className="px-4" style={{ borderLeft: `0.3rem solid ${titleBorderColor}` }}>
         {title}
       </span>
