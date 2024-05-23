84932403b32577a366e6b781db046a66a7a56f51
Samier Fabien <samierfabien@gmail.com>
2024-05-23 20:43:47 +0200

    Abandon des listes: effets indésirables+changement dernière section en Card

--- src/routes/Technologies.jsx
+++ src/routes/Technologies.jsx
@@ -4,7 +4,6 @@ import FooterTransition from "../components/FooterTransition";
 import HeaderTransition from "../components/HeaderTransition";
 import TitleOne from "../components/TitleOne";
 import Card from "../components/Card";
-import "../css/technologies.css";
 
 export default function Technologies() {
   const divRef = useRef(null);
@@ -12,10 +11,10 @@ export default function Technologies() {
   return (
     <>
       <HeaderTransition title="Technologies Employées" />
-      <div className="col p-0 sticky-top z-n1" ref={divRef}>
+      <div className="col p-0 sticky-top z-n1 mb-5" ref={divRef}>
         <BubblesContainer
           parentRef={divRef}
-          h={600}
+          h={800}
           framerate={40}
           speed={1.1}
           minRadius={30}
@@ -32,96 +31,107 @@ export default function Technologies() {
         />
       </div>
       <div className="container bg-transparent">
-        <div className="row justify-content-center">
-          <div className="col-xxl-6 col-xl-8 col-lg-10 text-dark">
-            <TitleOne title="Par type d'utilisation" titleBorderColor="var(--bs-primary)" />
-          </div>
-        </div>
-        <div className="row d-flex align-items-stretch">
+        <div className="row g-4">
           <Card
             headerTitle="Front-end"
             cardText={
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Bootstrap</li>
-                <li>React.js</li>
-                <li>Npm</li>
-              </ul>
+              <p>
+                HTML
+                <br />
+                CSS
+                <br />
+                JavaScript
+                <br />
+                Bootstrap
+                <br />
+                React.js
+                <br />
+                Npm
+                <br />
+              </p>
             }
+            theme="light"
           />
           <Card
             headerTitle="Back-end"
             cardText={
-              <ul>
-                <li>PHP</li>
-                <li>Composer</li>
-                <li>Symfony</li>
-                <li>Api Platform</li>
-              </ul>
+              <p>
+                <li className="list-group-item">PHP</li>
+                <li className="list-group-item">Composer</li>
+                <li className="list-group-item">Symfony</li>
+                <li className="list-group-item">Api Platform</li>
+              </p>
             }
+            theme="light"
           />
           <Card
             headerTitle="Appli traditionnelle"
             cardText={
-              <ul>
-                <li>Twig</li>
-                <li>JQuery</li>
-                <li>htmx</li>
-              </ul>
+              <p>
+                <li className="list-group-item">Twig</li>
+                <li className="list-group-item">JQuery</li>
+                <li className="list-group-item">htmx</li>
+              </p>
             }
+            theme="light"
           />
           <Card
             headerTitle="Outils de conception"
             cardText={
-              <ul>
-                <li>Trello</li>
-                <li>Figma</li>
-                <li>Draw.io (UML)</li>
-                <li>Looping-MCD (Merise)</li>
-              </ul>
+              <p>
+                <li className="list-group-item">Trello</li>
+                <li className="list-group-item">Figma</li>
+                <li className="list-group-item">Draw.io (UML)</li>
+                <li className="list-group-item">Looping-MCD (Merise)</li>
+              </p>
             }
+            theme="light"
           />
           <Card
             headerTitle="Développement"
             cardText={
-              <ul>
-                <li>VSCode</li>
-                <li>Docker</li>
-                <li>Docker compose</li>
-                <li>Git</li>
-                <li>Laragon / serveur PHP</li>
-                <li>Insomnia / Postman</li>
-                <li>Obsidian / Onenote</li>
-              </ul>
+              <p>
+                <li className="list-group-item">VSCode</li>
+                <li className="list-group-item">Docker</li>
+                <li className="list-group-item">Docker compose</li>
+                <li className="list-group-item">Git</li>
+                <li className="list-group-item">Laragon / serveur PHP (selon l'OS)</li>
+                <li className="list-group-item">Insomnia / Postman</li>
+                <li className="list-group-item">Obsidian / Onenote</li>
+              </p>
             }
+            theme="light"
           />
           <Card
             headerTitle="Design"
             cardText={
-              <ul>
-                <li>Inkscape /Illustrator</li>
-                <li>Gimp / Photoshop</li>
-                <li>Générateurs en ligne</li>
-              </ul>
+              <p>
+                <li className="list-group-item">Inkscape /Illustrator</li>
+                <li className="list-group-item">Gimp / Photoshop</li>
+                <li className="list-group-item">Générateurs en ligne</li>
+              </p>
             }
+            theme="light"
           />
         </div>
-        <div className="row justify-content-center">
-          <div className="col-xxl-6 col-xl-8 col-lg-10 text-dark">
-            <TitleOne title="Au fait, c'est quoi cette animation ?" />
-            <p>
-              C'est un algorithme que j'ai créé et qui utilise une balise canvas pour dessiner des
-              sortes de "bulles" qui se collisionnent. J'avais juste une idée en tête et après
-              m'être beaucoup documenté j'ai traduit les équations vers le code.
-              <br />
-              On appelle cela des "collisions élastiques", l'energie et la vitesse sont conservées.
-              Les "bulles" ont toutes un diamètre et une vitesse de base aléatoirement attribués
-              (dans une certaine plage). Cela leur confère un poids et une inertie différents en
-              fonction de leur vitesse.
-            </p>
-          </div>
+        <div className="row justify-content-center mt-4">
+          <Card
+            headerTitle="Au fait, c'est quoi cette animation ?"
+            cardText={
+              <p>
+                C'est un algorithme que j'ai créé et qui utilise une balise canvas pour dessiner des
+                sortes de "bulles" qui se collisionnent. J'avais juste une idée en tête et après
+                m'être beaucoup documenté j'ai traduit les équations vers le code.
+                <br />
+                On appelle cela des "collisions élastiques", l'energie et la vitesse sont
+                conservées. Les "bulles" ont toutes un diamètre et une vitesse de base aléatoirement
+                attribués (dans une certaine plage). Cela leur confère un poids et une inertie
+                différents en fonction de leur vitesse.
+              </p>
+            }
+            columnClasses="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"
+            theme="light"
+          />
         </div>
       </div>
       <FooterTransition />
