8c867172e31d516bd28708c90ea892a984c89347
Samier Fabien <samierfabien@gmail.com>
2024-05-23 17:41:21 +0200

    Ajout des Card dans Technologies

--- src/routes/Technologies.jsx
+++ src/routes/Technologies.jsx
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from "react";
 import BubblesContainer from "../components/bubbles/BubblesContainer";
 import FooterTransition from "../components/FooterTransition";
 import HeaderTransition from "../components/HeaderTransition";
+import TitleOne from "../components/TitleOne";
+import Card from "../components/Card";
 
 export default function Technologies() {
   const divRef = useRef(null);
@@ -32,11 +34,13 @@ export default function Technologies() {
         </div>
         <div className="row justify-content-center">
           <div className="col-xxl-6 col-xl-8 col-lg-10 text-dark">
-            <h2 className="display-6 pt-5">
-              <span className="px-4 border-start border-5 border-primary">Front-end.</span>
-            </h2>
-            <p>
-              Je travaille le plus couramment avec
+            <TitleOne title="Par type d'utilisation" titleBorderColor="var(--bs-primary)" />
+          </div>
+        </div>
+        <div className="row d-flex align-items-stretch">
+          <Card
+            headerTitle="Front-end"
+            cardText={
               <ul>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -45,58 +49,68 @@ export default function Technologies() {
                 <li>React.js</li>
                 <li>Npm</li>
               </ul>
-              Dans le cadre d'une application web "traditionnelle" dans laquelle le front et le back
-              sont gérés de manière monolithique, j'utilise
-              <ul>
-                <li>Twig</li>
-                <li>JQuery</li>
-                <li>htmx</li>
-              </ul>
-            </p>
-            <h2 className="display-6 pt-5">
-              <span className="px-4 border-start border-5 border-primary">Back-end.</span>
-            </h2>
-            <p>
-              Pour le back-end, j'utilise
+            }
+          />
+          <Card
+            headerTitle="Back-end"
+            cardText={
               <ul>
                 <li>PHP</li>
                 <li>Composer</li>
                 <li>Symfony</li>
                 <li>Api Platform</li>
               </ul>
-            </p>
-            <h2 className="display-6 pt-5">
-              <span className="px-4 border-start border-5 border-primary">Outils.</span>
-            </h2>
-            <p>
-              C'est avec Trello que je m'organise. Dans la conception je m'appuie sur Draw.io pour
-              l'UML, Looping-mcd pour la base de données (merise) et Figma pour le maquettage.
-              <br />
-              Je me sers de VSCode en guise d'éditeur de code. Avec l'aide de Docker et Docker
-              Compose je conteneurise mes applications qui sont versionnées avec Git.
-              <br />
-              Quand il m'arrive de tester des parties de code simples, j'utilise en fonction des cas
-              l'extension LiveServer de VSCode, Laragon sur Windows ou un serveur web interne PHP
-              sur Linux. Pour tester mes API Rest et GraphQL, j'utilise Insomnia ou Postman.
-              <br />
-              Mes éléments graphiques sont principalement produits avec Inkscape, et Gimp (qui ont
-              remplacé Photoshop et Illustrator) ainsi que de générateurs en ligne.
-              <br />
-              Enfin, en guise de base de connaissances, j'ai utilisé OneNote dans un contexte
-              professionnel. J'utilise pour ma part Obsidian.
-            </p>
-            <h2 className="display-6 pt-5">
-              <span className="px-4 border-start border-5 border-primary">Brèves incursions.</span>
-            </h2>
-            <p>
-              Python me permet de gérer certaines tâches répétitives. C++ et Qt pour un programme de
-              calcul de balourd notamment. Java que j'affectionne particulièrement.
-            </p>
-            <h2 className="display-6 pt-5">
-              <span className="px-4 border-start border-5 border-primary">
-                Au fait, c'est quoi cette animation ?
-              </span>
-            </h2>
+            }
+          />
+          <Card
+            headerTitle="Appli traditionnelle"
+            cardText={
+              <ul>
+                <li>Twig</li>
+                <li>JQuery</li>
+                <li>htmx</li>
+              </ul>
+            }
+          />
+          <Card
+            headerTitle="Outils de conception"
+            cardText={
+              <ul>
+                <li>Trello</li>
+                <li>Figma</li>
+                <li>Draw.io (UML)</li>
+                <li>Looping-MCD (Merise)</li>
+              </ul>
+            }
+          />
+          <Card
+            headerTitle="Développement"
+            cardText={
+              <ul>
+                <li>VSCode</li>
+                <li>Docker</li>
+                <li>Docker compose</li>
+                <li>Git</li>
+                <li>Laragon / serveur PHP</li>
+                <li>Insomnia / Postman</li>
+                <li>Obsidian / Onenote</li>
+              </ul>
+            }
+          />
+          <Card
+            headerTitle="Design"
+            cardText={
+              <ul>
+                <li>Inkscape /Illustrator</li>
+                <li>Gimp / Photoshop</li>
+                <li>Générateurs en ligne</li>
+              </ul>
+            }
+          />
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-xxl-6 col-xl-8 col-lg-10 text-dark">
+            <TitleOne title="Au fait, c'est quoi cette animation ?" />
             <p>
               C'est un algorithme que j'ai créé et qui utilise une balise canvas pour dessiner des
               sortes de "bulles" qui se collisionnent. J'avais juste une idée en tête et après
