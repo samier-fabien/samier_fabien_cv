6091ab5536a771fc573bbc4d4ef6076a5d46368f
Samier Fabien <samierfabien@gmail.com>
2024-05-30 15:45:38 +0200

    HeaderTransition: possibilité d'ajouter des classes à Transition+possibilité de changer la couleur du svg

--- src/components/HeaderTransition.jsx
+++ src/components/HeaderTransition.jsx
@@ -4,15 +4,20 @@ import Transition from "./Transition";
 export default function HeaderTransition({
   title = "",
   titleContainerClasses = "bg-dark text-light",
-  svgContainerClasses = "bg-dark",
-  svgFillColor = "#ffffff",
+  transitionContainerClasses = "bg-dark",
+  transitionFillColor = "#ffffff",
 }) {
   return (
     <>
       <div className={`container-fluid py-4 text-center ${titleContainerClasses}`}>
         <h1 className="display-4 m-0 p-0">{title}</h1>
       </div>
-      <Transition transitionShape="custom" transitionValues={[50, 100, 100, 349, -50, 50]} />
+      <Transition
+        transitionShape="custom"
+        transitionValues={[50, 100, 100, 349, -50, 50]}
+        transitionContainerClasses={transitionContainerClasses}
+        transitionFillColor={transitionFillColor}
+      />
     </>
   );
 }
