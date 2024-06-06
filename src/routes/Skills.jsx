483212d1d533ec2bc014740fd00c93ca00fe4709
Samier Fabien <samierfabien@gmail.com>
2024-06-06 13:25:03 +0200

    Création effet parallaxe

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -1,11 +1,59 @@
 import React from "react";
 import FooterTransition from "../components/FooterTransition";
 import HeaderTransition from "../components/HeaderTransition";
+import Transition from "../components/Transition";
+import "../css/skills.css";
 
 export default function Skills() {
   return (
     <>
-      <HeaderTransition title="Compétences" transitionContainerClasses="bg-dark sticky-top z-n1" />
+      <div className="container-fluid py-4 text-center bg-dark text-light">
+        <h1 className="display-4 m-0 p-0">Compétences</h1>
+      </div>
+      <Transition
+        transitionPathOnTop="true"
+        transitionShape="custom"
+        transitionValues={[50, 100, 100, 349, -50, 50]}
+        transitionContainerClasses="transition-background-1"
+        transitionFillColor="#202428"
+      />
+      <div className="sticky-top z-n1 bg-transparent">
+        <div className="parallax-container bg-parallax-1 sticky-top">
+          <div className="container">
+            <div className="row">
+              <h1>Lorem ipsum</h1>
+            </div>
+            <div className="row">
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique cum obcaecati
+                recusandae tempora optio et, nobis, dolorem cumque tempore illo esse nostrum
+                exercitationem reprehenderit. Iure ullam dolores magnam quae exercitationem.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Transition
+        transitionPathOnTop={0}
+        transitionContainerClasses="bg-transparent"
+        transitionFillColor="rgb(208, 208, 255)"
+      />
+      <div className="sticky-top z-n1 bg-transparent">
+        <div className="parallax-container bg-parallax-2 sticky-top">
+          <h1>Lorem ipsum</h1>
+        </div>
+      </div>
+
+      <Transition
+        transitionPathOnTop={0}
+        transitionContainerClasses="bg-transparent"
+        transitionFillColor="rgb(248, 222, 255)"
+      />
+      <div className="sticky-top z-n1 bg-transparent">
+        <div className="parallax-container bg-parallax-3 sticky-top">
+          <h1>Lorem ipsum</h1>
+        </div>
+      </div>
 
       <FooterTransition />
     </>
