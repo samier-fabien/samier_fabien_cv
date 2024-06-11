994f74dbf2a734e774dbd81109e7e32b37b5fdf8
Samier Fabien <samierfabien@gmail.com>
2024-06-11 16:13:17 +0200

    Mise en forme des titres dans les parallaxeContainer

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -21,7 +21,7 @@ export default function Skills() {
       />
       <ParallaxContainer containerClasses="bg-color-parallax-1">
         <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
-          <h1>Lorem ipsum</h1>
+          <h2 className="h1 parallax-title">Lorem ipsum</h2>
         </div>
         <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6 d-flex justify-content-center align-items-center">
           <RadialTextDisplay
@@ -49,7 +49,7 @@ export default function Skills() {
       />
       <ParallaxContainer containerClasses="bg-color-parallax-2">
         <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
-          <h1>Lorem ipsum</h1>
+          <h2 className="h1 parallax-title">Lorem ipsum</h2>
         </div>
         <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6 d-flex justify-content-center align-items-center">
           <RadialTextDisplay
@@ -77,7 +77,7 @@ export default function Skills() {
       />
       <ParallaxContainer containerClasses="bg-color-parallax-3">
         <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
-          <h1>Lorem ipsum</h1>
+          <h2 className="h1 parallax-title">Lorem ipsum</h2>
         </div>
         <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6 d-flex justify-content-center align-items-center">
           <RadialTextDisplay
