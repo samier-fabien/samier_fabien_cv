41990b93bb72b7bad073729c0dbc2fd7ab0fe0b2
Samier Fabien <samierfabien@gmail.com>
2024-06-12 13:39:19 +0200

    RadialTextElement: passage à la ligne du texte trop long

--- src/components/radialTextDisplay/RadialTextElement.jsx
+++ src/components/radialTextDisplay/RadialTextElement.jsx
@@ -10,6 +10,10 @@ export default function RadialTextElement({ index, text, cssClasses, xPosition, 
         left: `calc(50% + ${xPosition}px)`,
         top: `calc(50% - ${yPosition}px)`,
         transform: "translate(-50%, -50%)",
+        display: "inline-block",
+        maxWidth: "9rem",
+        wordWrap: "break-word",
+        whiteSpace: "normal",
       }}
     >
       {text}
