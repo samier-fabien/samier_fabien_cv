1822a4d7ecbe1c022b9f672199e802d1afd4837a
Samier Fabien <samierfabien@gmail.com>
2024-06-14 10:52:38 +0200

    Ajout des transitions dans la page

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Transition from "../components/Transition";
+import FooterTransition from "../components/FooterTransition";
 
 export default function Experiences() {
-  return <div>Experiences</div>;
+  return (
+    <>
+      <div className="container-fluid py-4 text-center bg-dark text-light">
+        <h1 className="display-4 m-0 p-0">Compétences</h1>
+      </div>
+      <Transition
+        transitionPathOnTop="true"
+        transitionShape="custom"
+        transitionValues={[50, 100, 100, 349, -50, 50]}
+        transitionContainerClasses="bg-white"
+        transitionFillColor="#202428"
+      />
+      <FooterTransition />
+    </>
+  );
 }
