b9f63011d515fe4c936bed5dc1c34d9e26c76fd6
Samier Fabien <samierfabien@gmail.com>
2024-06-14 18:35:43 +0200

    Ajout de DoubleSidedCard

---
+++ src/components/DoubleSidedCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function DoubleSidedCard() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">lorem</h5>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores architecto aliquam,
+          esse similique excepturi saepe velit nam, eveniet laborum porro, veniam hic magnam
+          tempora? Eos recusandae deleniti odio explicabo enim!
+        </p>
+      </div>
+    </div>
+  );
+}
