c820e79f1f17e253ab38c44eb6861a8d998064ee
Samier Fabien <samierfabien@gmail.com>
2024-06-19 13:04:21 +0200

    Stylisation des DoubleSidedCard

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Transition from "../components/Transition";
 import FooterTransition from "../components/FooterTransition";
 import DoubleSidedCard from "../components/DoubleSidedCard";
+import DoubleSidedCardContainer from "../components/DoubleSidedCardContainer";
 
 export default function Experiences() {
   return (
@@ -16,64 +17,89 @@ export default function Experiences() {
         transitionContainerClasses="bg-white"
         transitionFillColor="#202428"
       />
-      <div className="container">
-        <div className="row" style={{ height: "35rem" }}>
-          <div className="col">
-            <DoubleSidedCard
-              otherFace={
-                <div class="d-flex align-items-center h-100">
-                  <ul className="card-text">
-                    <li>Développement d'une application web pour la commune</li>
-                    <li>Choix des technologies: Symfony 5, Bootstrap, JQuery, Docker</li>
-                    <li>Conception de la base de données relationnelle (méthode merise)</li>
-                    <li>Maquettage avec Figma</li>
-                    <li>Modélisation UML</li>
-                    <li>Tests unitaires</li>
-                  </ul>
-                </div>
-              }
-            >
-              <div class="d-flex flex-column align-items-center justify-content-center h-100">
-                <h5 className="card-title">Collectivité territoriale d'Auchonvillers</h5>
-                <p className="card-text">Stage de développement web</p>
+      <DoubleSidedCardContainer>
+        <div className="col-xxl-6 col-xl-7 col-lg-8 col-md-11 col-sm-11 col-xs-12">
+          <DoubleSidedCard
+            otherFace={
+              <div className="d-flex align-items-center justify-content-center h-100">
+                <ul className="card-text list-group list-group-flush border-color-white">
+                  <li className="bg-orange-300 list-group-item  border-orange-100">
+                    Développement d'une application web pour la commune
+                  </li>
+                  <li className="bg-orange-300 list-group-item border-orange-100 text-gray-800">
+                    Choix des technologies: Symfony 5, Bootstrap, JQuery, Docker
+                  </li>
+                  <li className="bg-orange-300 list-group-item border-orange-100 text-gray-800">
+                    Conception de la base de données relationnelle (méthode merise)
+                  </li>
+                  <li className="bg-orange-300 list-group-item border-orange-100 text-gray-800">
+                    Maquettage avec Figma
+                  </li>
+                  <li className="bg-orange-300 list-group-item border-orange-100 text-gray-800">
+                    Modélisation UML
+                  </li>
+                  <li className="bg-orange-300 list-group-item border-orange-100 text-gray-800">
+                    Tests unitaires
+                  </li>
+                </ul>
               </div>
-            </DoubleSidedCard>
-          </div>
-          <div className="col">
-            <DoubleSidedCard
-              otherFace={
-                <div class="d-flex align-items-center h-100">
-                  <ul className="card-text">
-                    <li>Modifications d'APIs existantes</li>
-                    <li>Conception de bases de données</li>
-                    <li>
-                      Dialogue avec le personnel en vue de la réalisation d'algorithmes pour
-                      l'élaboration d'applications en lien avec leur travail
-                    </li>
-                    <li>
-                      Conception et développement d'APIs REST et GraphQL avec Symfony et Api
-                      Platform
-                    </li>
-                    <li>Développement front avec React, Jquery et Htmx</li>
-                    <li>Modification et développement de modules pour Prestashop</li>
-                    <li>Écriture de scripts en Bash et Python</li>
-                    <li>Documentation des applications créées</li>
-                    <li>
-                      Rédaction d'articles sur les diverses technologies rencontrées et employées
-                      avec Onenote en guise de base de connaissances
-                    </li>
-                  </ul>
-                </div>
-              }
-            >
-              <div class="d-flex flex-column align-items-center justify-content-center h-100">
-                <h5 className="card-title">Extérieurstock</h5>
-                <p className="card-text">Développement web</p>
-              </div>
-            </DoubleSidedCard>
-          </div>
+            }
+            cardClasses="bg-orange-300 border-5 border-orange-100"
+          >
+            <div className="d-flex flex-column align-items-center justify-content-center h-100">
+              <h2 className="card-title text-white">Collectivité territoriale d'Auchonvillers</h2>
+              <p className="card-text text-gray-800">Stage de développement web</p>
+            </div>
+          </DoubleSidedCard>
         </div>
-      </div>
+      </DoubleSidedCardContainer>
+      <DoubleSidedCardContainer>
+        <div className="col-xxl-6 col-xl-7 col-lg-8 col-md-11 col-sm-11 col-xs-12">
+          <DoubleSidedCard
+            otherFace={
+              <div className="d-flex align-items-center justify-content-center h-100">
+                <ul className="card-text list-group list-group-flush">
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Modifications d'APIs existantes
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Conception de bases de données
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Dialogue avec le personnel en vue de la réalisation d'algorithmes pour
+                    l'élaboration d'applications en lien avec leur travail
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Conception et développement d'APIs REST et GraphQL avec Symfony et Api Platform
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Développement front avec React, Jquery et Htmx
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Modification et développement de modules pour Prestashop
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Écriture de scripts en Bash et Python
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Documentation des applications créées
+                  </li>
+                  <li className="bg-indigo-200 list-group-item border-indigo-100 text-gray-800">
+                    Rédaction d'articles sur les diverses technologies rencontrées et employées avec
+                    Onenote en guise de base de connaissances
+                  </li>
+                </ul>
+              </div>
+            }
+            cardClasses="bg-indigo-200 border-5 border-indigo-100"
+          >
+            <div className="d-flex flex-column align-items-center justify-content-center h-100">
+              <h2 className="card-title text-white">Extérieurstock</h2>
+              <p className="card-text text-gray-800">Développement web</p>
+            </div>
+          </DoubleSidedCard>
+        </div>
+      </DoubleSidedCardContainer>
       <FooterTransition />
     </>
   );
