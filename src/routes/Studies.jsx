52ee5610931ccadaeb01d93bb301d4eb87c477a9
Samier Fabien <samierfabien@gmail.com>
2024-06-20 12:59:54 +0200

    Ajout du titre et de Transition en haut de page

--- src/routes/Studies.jsx
+++ src/routes/Studies.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Transition from "../components/Transition";
 
 export default function Studies() {
-  return <div>Studies</div>;
+  return (
+    <>
+      <div className="container-fluid py-4 text-center bg-dark text-light">
+        <h1 className="display-4 m-0 p-0">Études et formations</h1>
+      </div>
+      <Transition
+        transitionPathOnTop="true"
+        transitionShape="custom"
+        transitionValues={[50, 100, 100, 349, -50, 50]}
+        transitionContainerClasses="bg-white"
+        transitionFillColor="#202428"
+      />
+    </>
+  );
 }
