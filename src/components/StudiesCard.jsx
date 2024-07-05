579098fb43ee5ec65a54dbe4bd5f562d2ccf2eca
Samier Fabien <samierfabien@gmail.com>
2024-07-05 12:35:32 +0200

    Stylisation des dates et lieux + ajout de marge custom

--- src/components/StudiesCard.jsx
+++ src/components/StudiesCard.jsx
@@ -5,20 +5,20 @@ const StudiesCard = forwardRef(function StudiesCard(props, ref) {
   return (
     <div
       ref={ref}
-      className={`studies-card transition-duration-800 my-5 opacity-0 ${
+      className={`studies-card transition-duration-800 opacity-0 ${
         props.cssClasses ? props.cssClasses : ""
       }`}
     >
-      {props.title ? <h2 className="pb-4">{props.title}</h2> : ""}
-      {props.period ? <p className="text-end pb-4">{props.period}</p> : ""}
-      <p className="pb-1">{props.children}</p>
-      {props.place ? (
-        <p>
-          <em>{props.place}</em>
+      {props.title ? <h2 className="pb-4 text-white">{props.title}</h2> : ""}
+      {props.period ? (
+        <p className="text-end pb-4">
+          <em>{props.period}</em>
         </p>
       ) : (
         ""
       )}
+      <p className="pb-1">{props.children}</p>
+      {props.place ? <p className="fw-semibold">{props.place}</p> : ""}
     </div>
   );
 });
