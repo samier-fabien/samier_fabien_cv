8f4e9cf7ccda60df237146c63d52313f0129730e
Samier Fabien <samierfabien@gmail.com>
2024-07-10 15:47:55 +0200

    TitleOnobserved: renommage titleBorderColor >>> borderColor

--- src/components/TitleOneObserved.jsx
+++ src/components/TitleOneObserved.jsx
@@ -4,7 +4,7 @@ const TitleOneObserved = forwardRef(function TitleOneObserved(props, ref) {
   return (
     <h2
       ref={ref}
-      style={{ borderLeft: `0.3rem solid ${props.titleBorderColor}` }}
+      style={{ borderLeft: `0.3rem solid ${props.borderColor}` }}
       className={`display-6 ps-3 transition-duration-800 opacity-0 ${
         props.cssClasses ? props.cssClasses : ""
       }`}
