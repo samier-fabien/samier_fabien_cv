4d18151da912154aa73a302a0b19825308efeca5
Samier Fabien <samierfabien@gmail.com>
2024-07-10 15:22:55 +0200

    Ajout de TitleOneObserved.jsx

---
+++ src/components/TitleOneObserved.jsx
@@ -0,0 +1,16 @@
+import React, { forwardRef } from "react";
+
+const TitleOneObserved = forwardRef(function TitleOneObserved({ props, ref }) {
+  return (
+    <h2
+      ref={ref}
+      className={`display-6 pt-3 text-center transition-duration-800 ${
+        props.cssClasses ? props.cssClasses : ""
+      }`}
+    >
+      <span className="px-4" style={{ borderLeft: `0.3rem solid ${props.titleBorderColor}` }}>
+        {title}
+      </span>
+    </h2>
+  );
+});
