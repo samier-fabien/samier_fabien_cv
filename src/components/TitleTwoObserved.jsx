2b753e0e39dc436bc0e182398a75f9d3fb6a9173
Samier Fabien <samierfabien@gmail.com>
2024-07-10 16:16:06 +0200

    Correction sémantique html dans TitleTwoObserved

--- src/components/TitleTwoObserved.jsx
+++ src/components/TitleTwoObserved.jsx
@@ -2,14 +2,14 @@ import React, { forwardRef } from "react";
 
 const TitleTwoObserved = forwardRef(function TitleTwoObserved(props, ref) {
   return (
-    <h4
+    <h3
       ref={ref}
       className={`h4 pt-4 transition-duration-800 opacity-0 ${
         props.cssClasses ? props.cssClasses : ""
       }`}
     >
       {props.children}
-    </h4>
+    </h3>
   );
 });
 
