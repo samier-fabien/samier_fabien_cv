6dc6359a636bb2695755a026e1d9497ff79c9f06
Samier Fabien <samierfabien@gmail.com>
2024-07-10 16:51:03 +0200

    Déport de l'intersectionObserver dans ParagraphObserved.jsx

--- src/components/ParagraphObserved.jsx
+++ src/components/ParagraphObserved.jsx
@@ -1,11 +1,46 @@
-import React, { forwardRef } from "react";
+import React, { useEffect, useRef } from "react";
 
-const ParagraphObserved = forwardRef(function ParagraphObserved(props, ref) {
+export default function ParagraphObserved({ children, classToAdd, classToRemove, cssClasses }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio >= 1) {
+            displayElement(ref);
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  function displayElement(elementToObserve) {
+    const element = elementToObserve.current;
+    if (element.classList.contains(classToRemove)) {
+      element.classList.remove(classToRemove);
+      element.classList.add(classToAdd);
+    }
+  }
+
   return (
-    <p ref={ref} className={`transition-duration-800 ${props.cssClasses ? props.cssClasses : ""}`}>
-      {props.children}
+    <p ref={ref} className={`transition-duration-800 ${cssClasses ? cssClasses : ""}`}>
+      {children}
     </p>
   );
-});
-
-export default ParagraphObserved;
+}
