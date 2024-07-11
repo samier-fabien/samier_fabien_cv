81a6da5d1086e1a76bbfd056438844d94771ea57
Samier Fabien <samierfabien@gmail.com>
2024-07-11 17:57:44 +0200

    Changement de l'id du carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,14 +3,14 @@ import CarouselCard from "./CarouselCard";
 
 export default function Carousel({ slides }) {
   return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-theme="dark">
+    <div id="carouselTechnologies" className="carousel slide" data-bs-theme="dark">
       <div className="carousel-indicators">
         {slides
           ? slides.map((slide, index) =>
               index === 0 ? (
                 <button
                   type="button"
-                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-target="#carouselTechnologies"
                   data-bs-slide-to="0"
                   className="active"
                   aria-current="true"
@@ -19,7 +19,7 @@ export default function Carousel({ slides }) {
               ) : (
                 <button
                   type="button"
-                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-target="#carouselTechnologies"
                   data-bs-slide-to={index}
                   aria-label={`Slide ${index + 1}`}
                 ></button>
@@ -45,7 +45,7 @@ export default function Carousel({ slides }) {
       <button
         className="carousel-control-prev"
         type="button"
-        data-bs-target="#carouselExampleIndicators"
+        data-bs-target="#carouselTechnologies"
         data-bs-slide="prev"
       >
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -54,7 +54,7 @@ export default function Carousel({ slides }) {
       <button
         className="carousel-control-next"
         type="button"
-        data-bs-target="#carouselExampleIndicators"
+        data-bs-target="#carouselTechnologies"
         data-bs-slide="next"
       >
         <span className="carousel-control-next-icon text-dark" aria-hidden="true"></span>
