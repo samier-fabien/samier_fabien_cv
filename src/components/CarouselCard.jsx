0ede1cb8096356b6a729ef45a2adc95e132a622a
Samier Fabien <samierfabien@gmail.com>
2024-07-11 18:01:03 +0200

    CarouselCard: changement hauteur + card-body: contenu centré + suppression card-text

--- src/components/CarouselCard.jsx
+++ src/components/CarouselCard.jsx
@@ -8,13 +8,13 @@ export default function CarouselCard({
   cardClasses = "",
 }) {
   return (
-    <div className={`row justify-content-center my-5`} style={{ height: "22rem" }}>
+    <div className={`row justify-content-center my-5`} style={{ height: "50vh" }}>
       <div className={columnClasses}>
         <div className={`card shadow h-100 ${cardClasses}`}>
           <div className="card-header h2 text-white">{headerTitle}</div>
-          <div className="card-body">
+          <div className="card-body d-flex flex-column justify-content-center align-items-center">
             {cardTitle ? <h5 className="card-title">{cardTitle}</h5> : ""}
-            <p className="card-text">{cardText}</p>
+            {cardText}
           </div>
         </div>
       </div>
