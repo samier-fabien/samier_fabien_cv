0429a8be03b53a3403f6ef0738452b27d6f7ca16
Samier Fabien <samierfabien@gmail.com>
2024-09-23 16:34:38 +0200

    Ajout du chemin de base dans la configuration de vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/samier_fabien_cv/",
   build: {
     outDir: "docs",
   },
